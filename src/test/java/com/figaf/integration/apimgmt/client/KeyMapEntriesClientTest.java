84818cd00724d8a9bbff31f10884d5f4300b5300
Sergey Klochkov <skl@figaf.com>
2021-10-06 23:15:46 +0300

    IRT-2572 implemented tests for 'Delete' methods

--- src/test/java/com/figaf/integration/apimgmt/client/KeyMapEntriesClientTest.java
+++ src/test/java/com/figaf/integration/apimgmt/client/KeyMapEntriesClientTest.java
@@ -4,6 +4,7 @@ import com.figaf.integration.apimgmt.data_provider.AgentTestDataProvider;
 import com.figaf.integration.apimgmt.entity.KeyMapEntryMetaData;
 import com.figaf.integration.apimgmt.entity.KeyMapEntryValue;
 import com.figaf.integration.common.data_provider.AgentTestData;
+import com.figaf.integration.common.entity.RequestContext;
 import com.figaf.integration.common.factory.HttpClientsFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,6 +22,8 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 @Slf4j
 class KeyMapEntriesClientTest {
+
+    private static final String API_TEST_KEY_MAP_ENTRY_NAME = "FigafApiTestKeyMapEntry";
 
     private static KeyMapEntriesClient keyMapEntriesClient;
 
@@ -48,6 +52,45 @@ class KeyMapEntriesClientTest {
         keyMapEntryValues.get(1).setValue("value");
         keyMapEntryMetaData.setKeyMapEntryValues(keyMapEntryValues);
         keyMapEntriesClient.createOrUpdateKeyMapEntry(keyMapEntryMetaData, agentTestData.createRequestContext());
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(AgentTestDataProvider.class)
+    void test_privateKeyMapEntryApiDelete(AgentTestData agentTestData) {
+        RequestContext requestContext = agentTestData.createRequestContext(agentTestData.getTitle());
+        KeyMapEntryMetaData keyMapEntryMetaData = getOrCreateDummyKeyMapEntry(requestContext);
+        assertThat(keyMapEntryMetaData).as("Key map entry %s wasn't found", API_TEST_KEY_MAP_ENTRY_NAME).isNotNull();
+        keyMapEntriesClient.deleteKeyMapEntry(API_TEST_KEY_MAP_ENTRY_NAME, requestContext);
+        keyMapEntryMetaData = keyMapEntriesClient.getKeyMapEntryMetaData(API_TEST_KEY_MAP_ENTRY_NAME, requestContext);
+        assertThat(keyMapEntryMetaData).as("Key map entry %s wasn't deleted", API_TEST_KEY_MAP_ENTRY_NAME).isNull();
+    }
+
+    private KeyMapEntryMetaData createDummyKeyMapEntry(RequestContext requestContext) {
+        KeyMapEntryMetaData keyMapEntryMetaData = new KeyMapEntryMetaData();
+        keyMapEntryMetaData.setName(API_TEST_KEY_MAP_ENTRY_NAME);
+        keyMapEntryMetaData.setEncrypted(false);
+        keyMapEntryMetaData.setScope("ENV");
+
+        keyMapEntryMetaData.setKeyMapEntryValues(new ArrayList<>());
+        KeyMapEntryValue keyMapEntryValue = new KeyMapEntryValue(
+            API_TEST_KEY_MAP_ENTRY_NAME,
+            "key",
+            "value"
+        );
+        keyMapEntryMetaData.getKeyMapEntryValues().add(keyMapEntryValue);
+        keyMapEntriesClient.createOrUpdateKeyMapEntry(keyMapEntryMetaData, requestContext);
+        return keyMapEntriesClient.getKeyMapEntryMetaData(API_TEST_KEY_MAP_ENTRY_NAME, requestContext);
+    }
+
+    private KeyMapEntryMetaData getOrCreateDummyKeyMapEntry(RequestContext requestContext) {
+        KeyMapEntryMetaData keyMapEntryMetaData = keyMapEntriesClient.getKeyMapEntryMetaData(
+            API_TEST_KEY_MAP_ENTRY_NAME,
+            requestContext
+        );
+        if (keyMapEntryMetaData == null) {
+            keyMapEntryMetaData = createDummyKeyMapEntry(requestContext);
+        }
+        return keyMapEntryMetaData;
     }
 
 }
